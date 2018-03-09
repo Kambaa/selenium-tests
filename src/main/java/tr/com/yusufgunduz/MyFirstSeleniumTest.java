eda491243aa3b25ccbe61ab0ad5ccaa5facaf035
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2018-03-09 13:17:59 +0300

    fatjar update geckodriver and firefox binary file moved to the argument

--- src/main/java/tr/com/yusufgunduz/MyFirstSeleniumTest.java
+++ src/main/java/tr/com/yusufgunduz/MyFirstSeleniumTest.java
@@ -8,7 +8,10 @@ class MyFirstSeleniumTest {
     public static void main(String[] args) {
 
 //      download from https://github.com/mozilla/geckodriver/releases
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\ygndz\\Downloads\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", args[0]);
+        if (null != args[1]) {
+            System.setProperty("webdriver.firefox.bin", args[1]);
+        }
 
         WebDriver driver = new FirefoxDriver();
         driver.get("http://www.yusufgunduz.com.tr");
